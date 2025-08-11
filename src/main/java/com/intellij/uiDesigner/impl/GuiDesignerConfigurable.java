42a258bedac6201fddb0411d696c5f44130560da
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-08-11 22:04:29 +0400

    migrate to localize value

--- src/main/java/com/intellij/uiDesigner/impl/GuiDesignerConfigurable.java
+++ src/main/java/com/intellij/uiDesigner/impl/GuiDesignerConfigurable.java
@@ -32,206 +32,182 @@ import consulo.ui.layout.VerticalLayout;
 import consulo.ui.util.LabeledBuilder;
 import consulo.uiDesigner.impl.localize.UIDesignerLocalize;
 import consulo.util.lang.Comparing;
-import jakarta.inject.Inject;
-import jakarta.inject.Provider;
-
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
+import jakarta.inject.Inject;
+import jakarta.inject.Provider;
 
 /**
  * @author Anton Katilin
  * @author Vladimir Kondratyev
  */
 @ExtensionImpl
-public final class GuiDesignerConfigurable implements ProjectConfigurable, Configurable.NoScroll
-{
-	private static final Logger LOG = Logger.getInstance(GuiDesignerConfigurable.class);
+public final class GuiDesignerConfigurable implements ProjectConfigurable, Configurable.NoScroll {
+    private static final Logger LOG = Logger.getInstance(GuiDesignerConfigurable.class);
 
-	private final Project myProject;
-	private final Provider<GuiDesignerConfiguration> myGuiDesignerConfigurationProvider;
+    private final Project myProject;
+    private final Provider<GuiDesignerConfiguration> myGuiDesignerConfigurationProvider;
 
-	private MyLayout myGeneralUI;
+    private MyLayout myGeneralUI;
 
-	@Inject
-	public GuiDesignerConfigurable(final Project project, Provider<GuiDesignerConfiguration> guiDesignerConfigurationProvider)
-	{
-		myProject = project;
-		myGuiDesignerConfigurationProvider = guiDesignerConfigurationProvider;
-	}
+    @Inject
+    public GuiDesignerConfigurable(final Project project, Provider<GuiDesignerConfiguration> guiDesignerConfigurationProvider) {
+        myProject = project;
+        myGuiDesignerConfigurationProvider = guiDesignerConfigurationProvider;
+    }
 
-	@Nonnull
-	@Override
-	public String getDisplayName()
-	{
-		return UIDesignerBundle.message("title.gui.designer");
-	}
+    @Nonnull
+    @Override
+    public LocalizeValue getDisplayName() {
+        return UIDesignerLocalize.titleGuiDesigner();
+    }
 
-	@RequiredUIAccess
-	@Override
-	public Component createUIComponent(@Nonnull Disposable uiDisposable)
-	{
-		if(myGeneralUI == null)
-		{
-			myGeneralUI = new MyLayout();
-		}
+    @RequiredUIAccess
+    @Override
+    public Component createUIComponent(@Nonnull Disposable uiDisposable) {
+        if (myGeneralUI == null) {
+            myGeneralUI = new MyLayout();
+        }
 
-		return myGeneralUI.myPanel;
-	}
+        return myGeneralUI.myPanel;
+    }
 
-	@RequiredUIAccess
-	@Override
-	public boolean isModified()
-	{
-		if(myGeneralUI == null)
-		{
-			return false;
-		}
+    @RequiredUIAccess
+    @Override
+    public boolean isModified() {
+        if (myGeneralUI == null) {
+            return false;
+        }
 
-		GuiDesignerConfiguration configuration = myGuiDesignerConfigurationProvider.get();
+        GuiDesignerConfiguration configuration = myGuiDesignerConfigurationProvider.get();
 
-		if(myGeneralUI.myChkCopyFormsRuntime.getValueOrError() != configuration.COPY_FORMS_RUNTIME_TO_OUTPUT)
-		{
-			return true;
-		}
+        if (myGeneralUI.myChkCopyFormsRuntime.getValueOrError() != configuration.COPY_FORMS_RUNTIME_TO_OUTPUT) {
+            return true;
+        }
 
-		if(myGeneralUI.myChkCopyForms.getValueOrError() != configuration.COPY_FORMS_TO_OUTPUT)
-		{
-			return true;
-		}
+        if (myGeneralUI.myChkCopyForms.getValueOrError() != configuration.COPY_FORMS_TO_OUTPUT) {
+            return true;
+        }
 
-		if(!Comparing.equal(configuration.DEFAULT_LAYOUT_MANAGER, myGeneralUI.myLayoutManagerCombo.getValueOrError()))
-		{
-			return true;
-		}
+        if (!Comparing.equal(configuration.DEFAULT_LAYOUT_MANAGER, myGeneralUI.myLayoutManagerCombo.getValueOrError())) {
+            return true;
+        }
 
-		if(!Comparing.equal(configuration.DEFAULT_FIELD_ACCESSIBILITY, myGeneralUI.myDefaultFieldAccessibilityCombo.getValueOrError()))
-		{
-			return true;
-		}
+        if (!Comparing.equal(configuration.DEFAULT_FIELD_ACCESSIBILITY, myGeneralUI.myDefaultFieldAccessibilityCombo.getValueOrError())) {
+            return true;
+        }
 
-		if(configuration.INSTRUMENT_CLASSES != myGeneralUI.myRbInstrumentClasses.getValueOrError())
-		{
-			return true;
-		}
+        if (configuration.INSTRUMENT_CLASSES != myGeneralUI.myRbInstrumentClasses.getValueOrError()) {
+            return true;
+        }
 
-		if(configuration.RESIZE_HEADERS != myGeneralUI.myResizeHeaders.getValueOrError())
-		{
-			return true;
-		}
+        if (configuration.RESIZE_HEADERS != myGeneralUI.myResizeHeaders.getValueOrError()) {
+            return true;
+        }
 
-		if(configuration.USE_JB_SCALING != myGeneralUI.myUseJBScalingCheckBox.getValueOrError())
-		{
-			return true;
-		}
-		return false;
-	}
+        if (configuration.USE_JB_SCALING != myGeneralUI.myUseJBScalingCheckBox.getValueOrError()) {
+            return true;
+        }
+        return false;
+    }
 
-	@RequiredUIAccess
-	@Override
-	public void apply()
-	{
-		GuiDesignerConfiguration configuration = myGuiDesignerConfigurationProvider.get();
+    @RequiredUIAccess
+    @Override
+    public void apply() {
+        GuiDesignerConfiguration configuration = myGuiDesignerConfigurationProvider.get();
 
-		configuration.COPY_FORMS_RUNTIME_TO_OUTPUT = myGeneralUI.myChkCopyFormsRuntime.getValueOrError();
-		configuration.COPY_FORMS_TO_OUTPUT = myGeneralUI.myChkCopyForms.getValueOrError();
-		configuration.DEFAULT_LAYOUT_MANAGER = myGeneralUI.myLayoutManagerCombo.getValueOrError();
-		configuration.INSTRUMENT_CLASSES = myGeneralUI.myRbInstrumentClasses.getValueOrError();
-		configuration.DEFAULT_FIELD_ACCESSIBILITY = myGeneralUI.myDefaultFieldAccessibilityCombo.getValueOrError();
-		configuration.RESIZE_HEADERS = myGeneralUI.myResizeHeaders.getValueOrError();
-		configuration.USE_JB_SCALING = myGeneralUI.myUseJBScalingCheckBox.getValueOrError();
+        configuration.COPY_FORMS_RUNTIME_TO_OUTPUT = myGeneralUI.myChkCopyFormsRuntime.getValueOrError();
+        configuration.COPY_FORMS_TO_OUTPUT = myGeneralUI.myChkCopyForms.getValueOrError();
+        configuration.DEFAULT_LAYOUT_MANAGER = myGeneralUI.myLayoutManagerCombo.getValueOrError();
+        configuration.INSTRUMENT_CLASSES = myGeneralUI.myRbInstrumentClasses.getValueOrError();
+        configuration.DEFAULT_FIELD_ACCESSIBILITY = myGeneralUI.myDefaultFieldAccessibilityCombo.getValueOrError();
+        configuration.RESIZE_HEADERS = myGeneralUI.myResizeHeaders.getValueOrError();
+        configuration.USE_JB_SCALING = myGeneralUI.myUseJBScalingCheckBox.getValueOrError();
 
-		if(configuration.INSTRUMENT_CLASSES && !myProject.isDefault())
-		{
-			new RemovingSetupMethodProcessor(myProject).run();
-		}
-	}
+        if (configuration.INSTRUMENT_CLASSES && !myProject.isDefault()) {
+            new RemovingSetupMethodProcessor(myProject).run();
+        }
+    }
 
-	@RequiredUIAccess
-	@Override
-	public void reset()
-	{
-		if(myGeneralUI == null)
-		{
-			return;
-		}
+    @RequiredUIAccess
+    @Override
+    public void reset() {
+        if (myGeneralUI == null) {
+            return;
+        }
 
-		GuiDesignerConfiguration configuration = myGuiDesignerConfigurationProvider.get();
+        GuiDesignerConfiguration configuration = myGuiDesignerConfigurationProvider.get();
 
-		if(configuration.INSTRUMENT_CLASSES)
-		{
-			myGeneralUI.myRbInstrumentClasses.setValue(true);
-		}
-		else
-		{
-			myGeneralUI.myRbInstrumentSources.setValue(true);
-		}
-		myGeneralUI.myChkCopyFormsRuntime.setValue(configuration.COPY_FORMS_RUNTIME_TO_OUTPUT);
-		myGeneralUI.myChkCopyForms.setValue(configuration.COPY_FORMS_TO_OUTPUT);
+        if (configuration.INSTRUMENT_CLASSES) {
+            myGeneralUI.myRbInstrumentClasses.setValue(true);
+        }
+        else {
+            myGeneralUI.myRbInstrumentSources.setValue(true);
+        }
+        myGeneralUI.myChkCopyFormsRuntime.setValue(configuration.COPY_FORMS_RUNTIME_TO_OUTPUT);
+        myGeneralUI.myChkCopyForms.setValue(configuration.COPY_FORMS_TO_OUTPUT);
 
-		myGeneralUI.myLayoutManagerCombo.setValue(configuration.DEFAULT_LAYOUT_MANAGER);
+        myGeneralUI.myLayoutManagerCombo.setValue(configuration.DEFAULT_LAYOUT_MANAGER);
 
-		myGeneralUI.myDefaultFieldAccessibilityCombo.setValue(configuration.DEFAULT_FIELD_ACCESSIBILITY);
+        myGeneralUI.myDefaultFieldAccessibilityCombo.setValue(configuration.DEFAULT_FIELD_ACCESSIBILITY);
 
-		myGeneralUI.myResizeHeaders.setValue(configuration.RESIZE_HEADERS);
-		myGeneralUI.myUseJBScalingCheckBox.setValue(configuration.USE_JB_SCALING);
-	}
+        myGeneralUI.myResizeHeaders.setValue(configuration.RESIZE_HEADERS);
+        myGeneralUI.myUseJBScalingCheckBox.setValue(configuration.USE_JB_SCALING);
+    }
 
-	@RequiredUIAccess
-	@Override
-	public void disposeUIResources()
-	{
-		myGeneralUI = null;
-	}
+    @RequiredUIAccess
+    @Override
+    public void disposeUIResources() {
+        myGeneralUI = null;
+    }
 
-	private static class MyLayout
-	{
-		public VerticalLayout myPanel;
-		public RadioButton myRbInstrumentClasses;
-		public RadioButton myRbInstrumentSources;
-		public CheckBox myChkCopyFormsRuntime;
-		private ComboBox<String> myLayoutManagerCombo;
-		private ComboBox<String> myDefaultFieldAccessibilityCombo;
-		private CheckBox myResizeHeaders;
-		private CheckBox myChkCopyForms;
-		private CheckBox myUseJBScalingCheckBox;
+    private static class MyLayout {
+        public VerticalLayout myPanel;
+        public RadioButton myRbInstrumentClasses;
+        public RadioButton myRbInstrumentSources;
+        public CheckBox myChkCopyFormsRuntime;
+        private ComboBox<String> myLayoutManagerCombo;
+        private ComboBox<String> myDefaultFieldAccessibilityCombo;
+        private CheckBox myResizeHeaders;
+        private CheckBox myChkCopyForms;
+        private CheckBox myUseJBScalingCheckBox;
 
-		@RequiredUIAccess
-		private MyLayout()
-		{
-			myPanel = VerticalLayout.create();
+        @RequiredUIAccess
+        private MyLayout() {
+            myPanel = VerticalLayout.create();
 
-			Label label = Label.create(UIDesignerLocalize.labelGenerateGuiInto());
+            Label label = Label.create(UIDesignerLocalize.labelGenerateGuiInto());
 
-			ValueGroup<Boolean> group = ValueGroup.createBool();
-			myRbInstrumentClasses = RadioButton.create(UIDesignerLocalize.radioGenerateIntoClass()).toGroup(group);
-			myRbInstrumentSources = RadioButton.create(UIDesignerLocalize.radioGenerateIntoJava()).toGroup(group);
+            ValueGroup<Boolean> group = ValueGroup.createBool();
+            myRbInstrumentClasses = RadioButton.create(UIDesignerLocalize.radioGenerateIntoClass()).toGroup(group);
+            myRbInstrumentSources = RadioButton.create(UIDesignerLocalize.radioGenerateIntoJava()).toGroup(group);
 
-			myPanel.add(HorizontalLayout.create().add(DockLayout.create().top(label)).add(VerticalLayout.create().add(myRbInstrumentClasses).add(myRbInstrumentSources)));
+            myPanel.add(HorizontalLayout.create().add(DockLayout.create().top(label)).add(VerticalLayout.create().add(myRbInstrumentClasses).add(myRbInstrumentSources)));
 
-			myUseJBScalingCheckBox = CheckBox.create(LocalizeValue.localizeTODO("Use scaling util class (JBUI)"));
-			myPanel.add(myUseJBScalingCheckBox);
+            myUseJBScalingCheckBox = CheckBox.create(LocalizeValue.localizeTODO("Use scaling util class (JBUI)"));
+            myPanel.add(myUseJBScalingCheckBox);
 
-			myChkCopyFormsRuntime = CheckBox.create(UIDesignerLocalize.chkCopyFormRuntime());
-			myPanel.add(myChkCopyFormsRuntime);
+            myChkCopyFormsRuntime = CheckBox.create(UIDesignerLocalize.chkCopyFormRuntime());
+            myPanel.add(myChkCopyFormsRuntime);
 
-			myChkCopyForms = CheckBox.create(UIDesignerLocalize.chkCopyForm());
-			myPanel.add(myChkCopyForms);
+            myChkCopyForms = CheckBox.create(UIDesignerLocalize.chkCopyForm());
+            myPanel.add(myChkCopyForms);
 
-			myLayoutManagerCombo = ComboBox.create(LayoutManagerRegistry.getNonDeprecatedLayoutManagerNames());
-			myLayoutManagerCombo.selectFirst();
-			myLayoutManagerCombo.setTextRender(value -> LocalizeValue.of(LayoutManagerRegistry.getLayoutManagerDisplayName(value)));
+            myLayoutManagerCombo = ComboBox.create(LayoutManagerRegistry.getNonDeprecatedLayoutManagerNames());
+            myLayoutManagerCombo.selectFirst();
+            myLayoutManagerCombo.setTextRender(value -> LocalizeValue.of(LayoutManagerRegistry.getLayoutManagerDisplayName(value)));
 
-			myPanel.add(LabeledBuilder.sided(UIDesignerLocalize.defaultLayoutManager(), myLayoutManagerCombo));
-			
-			myDefaultFieldAccessibilityCombo = ComboBox.create("private", "package local", "protected", "public");
-			myDefaultFieldAccessibilityCombo.selectFirst();
+            myPanel.add(LabeledBuilder.sided(UIDesignerLocalize.defaultLayoutManager(), myLayoutManagerCombo));
 
-			myPanel.add(LabeledBuilder.sided(UIDesignerLocalize.defaultFieldAccessibility(), myDefaultFieldAccessibilityCombo));
+            myDefaultFieldAccessibilityCombo = ComboBox.create("private", "package local", "protected", "public");
+            myDefaultFieldAccessibilityCombo.selectFirst();
 
-			myResizeHeaders = CheckBox.create(LocalizeValue.localizeTODO("&Resize column and row headers with mouse"));
-			myPanel.add(myResizeHeaders);
-		}
-	}
+            myPanel.add(LabeledBuilder.sided(UIDesignerLocalize.defaultFieldAccessibility(), myDefaultFieldAccessibilityCombo));
+
+            myResizeHeaders = CheckBox.create(LocalizeValue.localizeTODO("&Resize column and row headers with mouse"));
+            myPanel.add(myResizeHeaders);
+        }
+    }
 
 //	private final class MyApplyRunnable implements Runnable
 //	{
@@ -298,17 +274,15 @@ public final class GuiDesignerConfigurable implements ProjectConfigurable, Confi
 //		}
 //	}
 
-	@Override
-	@Nonnull
-	public String getId()
-	{
-		return "project.propGUI";
-	}
+    @Override
+    @Nonnull
+    public String getId() {
+        return "project.propGUI";
+    }
 
-	@Nullable
-	@Override
-	public String getParentId()
-	{
-		return StandardConfigurableIds.EDITOR_GROUP;
-	}
+    @Nullable
+    @Override
+    public String getParentId() {
+        return StandardConfigurableIds.EDITOR_GROUP;
+    }
 }
